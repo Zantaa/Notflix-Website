5959aa34ddf6cd511fd2a9c900c0d34a190d157c
moirian12 <122929110+moirian12@users.noreply.github.com>
2025-02-07 18:43:04 -0500

    database connection successful

---
+++ pages/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<?php
+require_once "../includes/db.php";
+echo "Database connected successfully!";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
